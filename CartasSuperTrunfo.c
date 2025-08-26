68be6c7bcdf8c06e846df34fba7898fc67b9310e
franciscofilhomc-dev <franciscofilhomc@gmail.com>
2025-08-26 18:59:31 +0000

    alterei o CartasSuperTrunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,16 +1,33 @@
 #include <stdio.h>
-int main {
-DESAFIO Super Trunfo-paises
-Tema  1
-  char estado ;
-  char codigo  da carta;
-  char  nome  da  cidade
-  int população
+int main () {
+  char estado;
+  char codigo_da_carta;
+  char  nome_da_cidade;
+  int população;
+  float aréa_em_km²;
+  float PIB;
+  int numero_de_pontos_turisticos;
+ 
+     printf("digite  seu estado:");
+     scanf("%s",estado);
+      
+     printf("digite o codigo  da  carta:");
+    scanf("%s",&codigo_da_carta );
+ 
+     printf("digite o nome da cidade: ");
+     scanf("%s",&nome_da_cidade);
+ 
+     ;printf("digite a sua  população:")
+     ;scanf("%d" ,&população);
+ 
+     printf("digite  a sua area  em  km²:");
+     scanf("%f",&aréa_em_km²);
 
+    ;printf("digite seu PIB:");
+    scanf("%f",&PIB);
 
-  
+    printf("digite  o numero de  pontos turisticos:");
+    ;scanf("%d",&numero_de_pontos_turisticos);
 
-
-
-
+return 0;
 }
